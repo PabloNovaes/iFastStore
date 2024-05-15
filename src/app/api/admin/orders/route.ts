2b1ea1a2d92462c52090b316b7cd5963e43a9b57
PabloNovaes <ppablo.0028922@gmail.com>
2024-05-15 17:22:49 -0300

    update deployment config

--- src/app/api/admin/orders/route.ts
+++ src/app/api/admin/orders/route.ts
@@ -3,6 +3,7 @@ import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "../../../../../prisma/client";
 
+export const dynamic = "force-dynamic"
 export async function GET(req: NextRequest) {
     try {
         const { sessionClaims } = auth();
