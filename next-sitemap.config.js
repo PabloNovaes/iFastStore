8e00fb9e64f902840cab66df4eaac9c32e2d9b7f
Pablo Novaes <133129982+PabloNovaes@users.noreply.github.com>
2024-06-30 23:54:28 -0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    siteUrl: "https://fast-store-test.vercel.app/",
+    siteUrl: "https://ifaststore.it/",
     generateRobotsTxt: true,
     exclude: ['/server-sitemap-index.xml'],
     robotsTxtOptions: {
         additionalSitemaps: [
-            'https://fast-store-test.vercel.app/server-sitemap-index.xml'
+            'https://ifaststore.it/'
         ]
     }
 }
