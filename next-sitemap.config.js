d955b387242ebc868e35d833dcb46eada407ab53
Pablo Novaes <133129982+PabloNovaes@users.noreply.github.com>
2024-07-16 14:28:33 -0300

    Atualizar o next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,11 +6,37 @@ module.exports = {
         policies: [
             {
                 userAgent: '*',
-                allow: '/'
+                allow: '/',
+                disallow: ['/dashboard/']
             }
         ],
         additionalSitemaps: [
             'https://ifaststore.it/sitemap.xml'
         ]
+    },
+    transform: async (config, url) => {
+        // Adicione URLs específicas ao sitemap
+        const additionalUrls = [
+            '/products',
+            '/products/category/iphone',
+            '/products/category/airpods',
+            '/products/category/notebooks'
+        ];
+
+        if (additionalUrls.some(path => url.includes(path))) {
+            return {
+                loc: url,
+                lastmod: new Date().toISOString(),
+                changefreq: 'monthly',
+                priority: 0.7
+            };
+        }
+
+        return {
+            loc: url,
+            lastmod: new Date().toISOString(),
+            changefreq: 'daily',
+            priority: 0.5
+        };
     }
 }
