5b898f77e4cd5f2a18cf00ee1634919b1c201c30
Pablo Novaes <133129982+PabloNovaes@users.noreply.github.com>
2024-10-13 12:05:39 -0300

    Update ProductColorSelector.tsx

--- src/components/ProductColorSelector.tsx
+++ src/components/ProductColorSelector.tsx
@@ -19,7 +19,7 @@ export function ProductColorSelector({ colors, handleSetActiveColor, inStock, ha
                 colors.map(({ name, code, available }) => {
                     return (
                         <RadioGroupItem disabled={!available || !inStock || hasModelSelected === null} key={name} id={name} value={name} onClick={() => handleSetActiveColor(name)}
-                            className="border rounded-full shadow-sm p-1 px-2 flex gap-2 items-center radio-item overflow-hidden  data-[state=checked]:border-blue-400 transition-all disabled:opacity-50 disabled:cursor-not-allowed relative">
+                            className="border rounded-full bg-muted/40 shadow-sm p-1 px-2 flex gap-2 items-center radio-item overflow-hidden  data-[state=checked]:border-blue-400 transition-all disabled:opacity-50 disabled:cursor-not-allowed relative">
                             <div className="rounded-full size-4 shadow-black/50 shadow-inner" style={{ background: code }}></div>
                             <span className="text-sm select-none">{name}</span>
                         </RadioGroupItem>
@@ -29,7 +29,7 @@ export function ProductColorSelector({ colors, handleSetActiveColor, inStock, ha
                 colors.map(({ name, code, available }) => {
                     return (
                         <RadioGroupItem disabled={!inStock || !available}  key={name} id={name} value={name} onClick={() => handleSetActiveColor(name)}
-                            className="border rounded-full shadow-sm p-1 px-2 flex gap-2 items-center radio-item overflow-hidden  data-[state=checked]:border-blue-400 transition-all disabled:opacity-50 disabled:cursor-not-allowed relative">
+                            className="border rounded-full bg-muted/40 shadow-sm p-1 px-2 flex gap-2 items-center radio-item overflow-hidden  data-[state=checked]:border-blue-400 transition-all disabled:opacity-50 disabled:cursor-not-allowed relative">
                             <div className="rounded-full size-4 shadow-black/50 shadow-inner" style={{ background: code }}></div>
                             <span className="text-sm select-none">{name}</span>
                         </RadioGroupItem>
