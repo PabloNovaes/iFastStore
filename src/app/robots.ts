5849349eafc0b8875cc3ae5902e282fa83e9b0f2
PabloNovaes <ppablo.0028922@gmail.com>
2026-04-24 09:22:50 -0300

    (chore): update middleware and robots .ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "dashboard"],
+      disallow: ["/api", "/dashboard"],
     },
     sitemap: "https://ifaststore.it/sitemap.xml",
   };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,25 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/products(.*)",
+  "/sitemap.xml",
+  "/robots.txt",
+  "/not-found",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+    "/(api|trpc)(.*)",
+  ],
 };
